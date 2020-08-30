117fab627d183269ea1e841a1775a305fcc8560f
AskmienFoodoe <bowen@dragonjiang.com>
2020-08-30 18:04:49 -0400

    Add sonolus converter

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { ChartStore } from './contexts/ChartContext'
+import { BrowserRouter, Route } from 'react-router-dom'
+import SonolusConverter from './components/SonolusConverter';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChartStore>
-      <App />
+      <BrowserRouter>
+        <Route path="/" exact component={App} />
+        <Route path="/sonolus-converter" component={SonolusConverter} />
+      </BrowserRouter>
     </ChartStore>
   </React.StrictMode>,
   document.getElementById('root')
