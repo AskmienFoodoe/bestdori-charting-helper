b850fa65c96022591494245a6c32f8a553ed65ce
AskmienFoodoe <bowen@dragonjiang.com>
2020-09-01 14:09:28 -0400

    Add loading spinner to chart input

--- src/components/ChartInput.tsx
+++ src/components/ChartInput.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Form, TextArea, Input, Dropdown, Label, DropdownProps, InputOnChangeData, Button, ButtonProps, Message } from 'semantic-ui-react'
+import { Form, TextArea, Input, Dropdown, Label, DropdownProps, InputOnChangeData, Button, ButtonProps, Message, Dimmer, Loader } from 'semantic-ui-react'
 import { Chart } from '../common/Chart'
 import ChartContext from '../contexts/ChartContext'
 import { client } from '../common/api'
@@ -21,6 +21,7 @@ type stateType = {
     source: string
     chartId: string
     difficulty: string
+    loading: boolean
     errorMessage: string
 }
 
@@ -34,6 +35,7 @@ export default class ChartInput extends React.Component<propsType, stateType> {
         source: 'local',
         chartId: '',
         difficulty: 'expert',
+        loading: false,
         errorMessage: ''
     }
 
@@ -71,23 +73,24 @@ export default class ChartInput extends React.Component<propsType, stateType> {
         }
     }
 
-    getChartFromApi = async () => {
-        const source = this.state.source
-        try {
-            let response, chart
-            if (source === 'bandori') {
-                response = await client.get(`/songs/chart/${this.state.chartId}.${this.state.difficulty}.json`)
-                chart = response.data
-            } else if (source === 'bestdori') {
-                response = await client.get(`/post/details?id=${this.state.chartId}`)
-                chart = response.data.post.notes
+    getChartFromApi = () => {
+        this.setState({ loading: true }, async() => {
+            const source = this.state.source
+            try {
+                let response, chart
+                if (source === 'bandori') {
+                    response = await client.get(`/songs/chart/${this.state.chartId}.${this.state.difficulty}.json`)
+                    chart = response.data
+                } else if (source === 'bestdori') {
+                    response = await client.get(`/post/details?id=${this.state.chartId}`)
+                    chart = response.data.post.notes
+                }
+                const newChart = new Chart(chart)
+                this.setState({ inputChart: JSON.stringify(newChart.chartElements), errorMessage: '', loading: false }, this.updateChartInContext)
+            } catch (err) {
+                this.setState({ errorMessage: requestError, loading:false })
             }
-            const newChart = new Chart(chart)
-            this.setState({ inputChart: JSON.stringify(newChart.chartElements), errorMessage: '' }, this.updateChartInContext)
-        } catch (err) {
-            this.setState({ errorMessage: requestError })
-        }
-
+        })
     }
 
     componentDidMount() {
@@ -110,7 +113,7 @@ export default class ChartInput extends React.Component<propsType, stateType> {
                         <Button content='Ex' difficulty='expert' color={this.state.difficulty === 'expert' ? 'red' : undefined} onClick={this.handleDifficultyChange} />
                         <Button content='Sp' difficulty='special' color={this.state.difficulty === 'special' ? 'pink' : undefined} onClick={this.handleDifficultyChange} />
                     </Button.Group>
-                    <Message warning header='Warning!' content='Charts where straight holds and slides overlap may not parse correctly because trying to figure out how to convert from Longs to Slides makes me want to go commit die'/>
+                    <Message warning header='Warning!' content='Charts where straight holds and slides overlap may not parse correctly because trying to figure out how to convert from Longs to Slides makes me want to go commit die' />
                 </>
             )
         } else if (source === 'bestdori') {
@@ -126,6 +129,9 @@ export default class ChartInput extends React.Component<propsType, stateType> {
         return (
             <Form warning error={!!this.state.errorMessage.length}>
                 <h5>Source</h5>
+                <Dimmer inverted active={this.state.loading}>
+                    <Loader size='massive' />
+                </Dimmer>
                 <Form.Field inline>
                     <Label style={{ fontSize: '16px' }}>
                         <Dropdown options={options} value={this.state.source} onChange={this.handleDropdownChange} />
